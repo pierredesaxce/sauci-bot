29dc61735361430cd21547bb9c60ba0d752de57d
Glitch (hello-express) <none>
2021-02-26 17:19:25 +0000

    🌦⚡️ Checkpoint ./package.json:29104441/2642 ./server.js:29104441/1679 ./config.json:29104441/176

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.send("DISCORD YT NOTIFIER");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+const discord = require("discord.js");
+const client = new discord.Client();
+const { TOKEN, Channel_ID} = require("./config.json")
+const YoutubeNotifier = require("youtube-notification");
+
+
+client.on("ready", () =>{
+  console.log("w")
+})
+
+
+const notifier = new YouTubeNotifier({
+  hubCallback: 'https://fearless-turquoise-devourer.glitch.me/',
+  secret: 'Saucisson_Will_Prevail',
+});
+
+notifier.on('notified', data => {
+  console.log('New Video');
+  console.log(
+    `${data.channel.name} just uploaded a new video titled: ${data.video.title}`
+  );
+});
+ 
+notifier.subscribe('channel_1');
+
+app.use("/yt", notifier.listener());
+
+client.login()

72455e446ba41ae385c1a36919063f1a4d23ee90
Glitch (hello-express) <none>
2021-02-26 17:31:19 +0000

    📟☄️ Checkpoint ./server.js:29104441/25300 ./config.json:29104441/101

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
+const http = require("http");
 const express = require("express");
 const app = express();
-
-
-app.use(express.static("public"));
+var server = http.createServer(app);
 
 app.get("/", (request, response) => {
-  response.send("DISCORD YT NOTIFIER");
+  console.log(`Ping Received.`);
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("DISCORD BOT YO");
+});
+
+const listener = server.listen(process.env.PORT, function() {
+  console.log(`Your app is listening on port ` + listener.address().port);
 });
 
 
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
-
-const discord = require("discord.js");
-const client = new discord.Client();
-const { TOKEN, Channel_ID} = require("./config.json")
-const YoutubeNotifier = require("youtube-notification");
+const discord = require("discord.js")
+const client = new discord.Client()
+const { TOKEN, CHANNEL_ID, SERVER_CHANNEL_ID } = require("./config.json");
+const YouTubeNotifier = require('youtube-notification');
 
 
-client.on("ready", () =>{
-  console.log("w")
+client.on("ready", () => {
+  console.log("Watching " + CHANNEL_ID.length  + " Channels")
 })
 
-
 const notifier = new YouTubeNotifier({
-  hubCallback: 'https://fearless-turquoise-devourer.glitch.me/',
-  secret: 'Saucisson_Will_Prevail',
+  hubCallback: 'https://necessary-probable-slouch.glitch.me/yt',
+  secret: 'JOIN_MY_SERVER_OR_DIE'
 });
+
 
 notifier.on('notified', data => {
   console.log('New Video');
-  console.log(
-    `${data.channel.name} just uploaded a new video titled: ${data.video.title}`
+  client.channels.cache.get(SERVER_CHANNEL_ID).send(
+    `**${data.channel.name}** just uploaded a new video - **${data.video.link}**`
   );
 });
  
-notifier.subscribe('channel_1');
+notifier.subscribe(CHANNEL_ID);
 
 app.use("/yt", notifier.listener());
 
-client.login()
+
+client.login(TOKEN)
